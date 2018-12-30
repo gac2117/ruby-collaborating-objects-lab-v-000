e79b5cf77ef5095df497dad6b9574839d5297489
Grace Choi <oiograceoio@gmail.com>
2018-12-30 06:58:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,10 +11,6 @@ class Artist
     @@all
   end
 
-  def name
-
-  end
-
   def self.find_or_create_by_name(name)
     self.all.find{|artist| artist.name == name} || self.new(name).tap{|artist| artist.save}
   end
