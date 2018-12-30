50e66326a6afe0f00c702d9c0cea2011d9f86a30
Grace Choi <oiograceoio@gmail.com>
2018-12-30 06:58:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,26 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
+  def self.all
+    @@all 
+  end
+  
   def name
 
   end
   
   def self.find_or_create_by_name(name)
-
+    self.all.find{|artist| artist.name == name} || self.new(name).tap{|artist| artist.save}
   end
 
   def add_song(song)
-
+    @songs << song 
   end
 
   def save
