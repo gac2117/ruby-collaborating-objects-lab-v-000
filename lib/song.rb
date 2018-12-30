e8e45b4a6334cb4e8427cf92510226b05ce2a5b6
Grace Choi <oiograceoio@gmail.com>
2018-12-30 06:50:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,15 +9,12 @@ class Song
   def self.new_by_filename(filename)
     song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist_name = artist 
     new_song
   end
 
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end
