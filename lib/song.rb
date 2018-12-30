66a3172b4ea22f901da965247cae623f2d4e6c24
Grace Choi <oiograceoio@gmail.com>
2018-12-30 07:12:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = filename.split(" - ")
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
